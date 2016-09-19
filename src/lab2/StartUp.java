06c7fe789c81b96af9747e4473731519c08c3a5e
jstra <jstra@10.30.20.202>
2016-09-19 16:45:10 -0500

    added working start up

--- src/lab2/StartUp.java
+++ src/lab2/StartUp.java
@@ -3,20 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab1;
+package lab2;
 
 /**
  *
  * @author jstra
  */
 public class StartUp {
+    
     public static void main(String[] args) {
-        ProgrammingCourse course1 = new IntroToProgrammingCourse("Introduction to Programming","123-456");
-        ProgrammingCourse course2 = new IntroToProgrammingCourse("Introduction to Java","123-123");
-        ProgrammingCourse course3 = new AdvancedJavaCourse("Advanced Java","456-456");
+        lab1.ProgrammingCourse course1 = new lab1.IntroToProgrammingCourse("Introduction to Programming","123-456");
+        lab1.ProgrammingCourse course2 = new lab1.IntroToProgrammingCourse("Introduction to Java","123-123");
+        lab1.ProgrammingCourse course3 = new lab1.AdvancedJavaCourse("Advanced Java","456-456");
         
         System.out.println("Class Name: "+course1.getCourseName()+", Class Number: "+course1.getCourseNumber());
         System.out.println("Class Name: "+course2.getCourseName()+", Class Number: "+course2.getCourseNumber());
         System.out.println("Class Name: "+course3.getCourseName()+", Class Number: "+course3.getCourseNumber());
     }
-}
+
+}   
