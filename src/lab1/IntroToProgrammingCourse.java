55573c46340d538b1c13e3034ed05f67778105b4
jstra <jstra@10.30.20.202>
2016-09-19 16:33:19 -0500

    small fix

--- src/lab1/IntroToProgrammingCourse.java
+++ src/lab1/IntroToProgrammingCourse.java
@@ -17,17 +17,7 @@ public class IntroToProgrammingCourse extends ProgrammingCourse {
         setCourseNumber(courseNumber);
     }
 
-    @Override
     
-    public void setPrerequisites(String prerequisites) {
-        if(prerequisites == null || prerequisites.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: prerequisites cannot be null of empty string");
-            System.exit(0);
-        }
-        
-    }
-   
 
     
 
